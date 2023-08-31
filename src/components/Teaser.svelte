d3c3b0626e4f8da9ad5e171da65d7b2b6823a8c8
Josefine Schaefer <schaeferj@hotmail.de>
2023-08-31 15:51:45 +0200

    add global headline styling

--- src/components/Teaser.svelte
+++ src/components/Teaser.svelte
@@ -7,7 +7,7 @@
 <svelte:element
 	this={blok.headline_level}
 	use:storyblokEditable={blok}
-	class="py-32 text-6xl text-[#772D8B] font-bold text-center"
+	class="py-32 font-bold text-center"
 >
 	{blok.headline}
 </svelte:element>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,6 @@
 </script>
 
 <main>
+  <h1 class="py-32 text-6xl text-[#772D8B] font-bold text-center">Manually controlled Headline Hierarchies</h1>
   <slot />
 </main>
